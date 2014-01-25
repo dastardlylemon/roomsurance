05775b8278756ecc04b788a6db706fda7ebbef40
Eileen <eileenthe@gmail.com>
2014-01-25 06:35:14 -0800

    moved html to php file

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
 
 
 ?>
+<div data-role="page" id="login" class="card">
+            <div data-role="content">
+                <script type="text/javascript" src="index.js"></script>
+                <h1>roomsurance</h1>
+                <div id="fb-root"><a href="#home" onclick="fb_login();"><button class="login">LOG IN GODDAMMIT</button></a></div>
+            </div>
+        </div>
