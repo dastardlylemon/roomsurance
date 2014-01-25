7acba8b885628556c66f3eca12035b48a0d824c3
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 08:34:30 -0800

    More vardumps

--- test.php
+++ test.php
@@ -4,11 +4,11 @@ require_once ('./db_connect.php');
 
 echo "test";
 
-mysql_query("INSERT INTO groups
-VALUES (13, 'Kantai Collection',1000, 40000, 'kancolle')");
+var_dump(mysql_query("INSERT INTO groups
+VALUES (13, 'Kantai Collection',1000, 40000, 'kancolle')"));
 
-mysql_query("INSERT INTO users
-VALUES ('1000', 'Yuudachi', 13, 20, 100.50)");
+var_dump(mysql_query("INSERT INTO users
+VALUES ('1000', 'Yuudachi', 13, 20, 100.50)"));
 
 mysql_close($con);
 
