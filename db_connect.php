549d4bc95bfa699074f352ecb37dad678a45e7e1
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 08:39:01 -0800

    Substring tomfoolery

--- db_connect.php
+++ db_connect.php
@@ -3,8 +3,8 @@
   $server = "us-cdbr-east-04.cleardb.com";
   $username = "bd925a77ad68a1";
   $password = "88d26dab";
-  $db = substr("heroku_b2b5817a2ac0cf5",1);
+  $db = "heroku_b2b5817a2ac0cf5";
 
-  $con = mysql_connect($server, $username, $password);
+  mysql_connect($server, $username, $password);
 
   mysql_select_db($db);
--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require_once ('./db_connect.php');
 echo "test";
 
 var_dump(mysql_query("INSERT INTO groups
-VALUES (13, 'Kantai Collection',1000, 40000, 'kancolle')"));
+VALUES (13, 'Kantai Collection', 1000, 40000, 'kancolle')"));
 
 var_dump(mysql_query("INSERT INTO users
 VALUES ('1000', 'Yuudachi', 13, 20, 100.50)"));
