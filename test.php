7ef975f5a55c237795fe58cc9524729e5ae506fd
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 08:11:33 -0800

    Removed con

--- test.php
+++ test.php
@@ -1,12 +1,8 @@
 <?php
 echo "test2";
 require_once ('./db_connect.php');
-$con = mysqli_connect($server, $username, $password, $db);
+
 echo "test";
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
 mysqli_query($con,"INSERT INTO groups
 VALUES (13, 'Kantai Collection',1000, 40000, 'kancolle')");
