45c08fcf07a5217dac75583657500ed19d7f81e7
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 08:30:46 -0800

    mysql SUCKS

--- test.php
+++ test.php
@@ -1,14 +1,13 @@
 <?php
 echo "test2";
 require_once ('./db_connect.php');
-var_dump ($con);
 
 echo "test";
 
-mysql_query($con,"INSERT INTO groups
+mysql_query("INSERT INTO groups
 VALUES (13, 'Kantai Collection',1000, 40000, 'kancolle')");
 
-mysql_query($con,"INSERT INTO users
+mysql_query("INSERT INTO users
 VALUES ('1000', 'Yuudachi', 13, 20, 100.50)");
 
 mysql_close($con);
