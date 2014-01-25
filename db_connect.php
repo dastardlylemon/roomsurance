86dd7fab817fa5bfd1ef51d50bb8eca94f953d5a
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 08:25:22 -0800

    Changed how we call db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,9 @@
 <?php
-  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
   
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"],1);
+  $server = "us-cdbr-east-04.cleardb.com";
+  $username = "bd925a77ad68a1";
+  $password = "88d26dab";
+  $db = substr("heroku_b2b5817a2ac0cf5",1);
 
   $con = mysql_connect($server, $username, $password);
 
