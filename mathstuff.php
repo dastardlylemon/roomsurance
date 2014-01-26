c45810964926f9a558c39f2b2711c1777f2b7dfe
Soomin Jeong <sjeongus@gmail.com>
2014-01-26 05:20:18 -0800

    Mathstuff, more like....math...stuff

--- mathstuff.php
+++ mathstuff.php
@@ -3,8 +3,8 @@
 
   function Chores($dueDate, $pointVal, ,$complete, $personAsgn, $peronDid)
   {
-  	$today = date('Ymd');
-  	if($today<$dueDate && $complete == 0)
+  	$today = date('Y-m-d');
+  	if(($today < $dueDate) && ($complete == 0)
   		$personAsgnPoints -= $pointVal;
   	if($complete == 1 && (strcmp($personAsgn, $peronDid) != 0))
   		$peronDidPoints += $pointVal;
