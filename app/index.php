47d3e893b80050bf8f47fed0919a29c652facab2
dastardlylemon <jwu@efunda.com>
2014-01-26 04:29:40 -0800

    testing

--- app/index.php
+++ app/index.php
@@ -13,8 +13,8 @@ function getChores($groupID)
 function printChores($groupID){
   $array = getChores($groupID);
   for ($i = 0; $i < count($array); $i++) {
-    echo '<div class="chore-date">' . date('m/d', strtotime($array[$i][due_date])) .
-      '<div class="chore" id ="choreID' .$array[$i][choreid] . '">' .
+    echo '<div class="chore" id ="choreID' .$array[$i][choreid] . '">' .
+      '<div class="chore-date">' . date('m/d', strtotime($array[$i][due_date])) . '</div>' .
       '<div class="chore-title">' . $array[$i][chore_name] . ' </div>' .  
       '<div class="chore-desc">' . $array[$i][chore_descrip] .' </div>' .
       '<div class="chore-diff">' . $array[$i][difficulty] . ' </div>' .   
