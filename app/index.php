acf9299f2edc30a5507200012288a6143570b1e7
dastardlylemon <jwu@efunda.com>
2014-01-26 05:35:43 -0800

    group name update

--- app/index.php
+++ app/index.php
@@ -31,7 +31,10 @@ function printChores($groupID){
 function getGroupName($groupID){
   $findGroup = "SELECT group_name FROM groups WHERE groupid = " . $groupID;
   $res = mysql_query($findGroup);
-  return $res[0];
+  while ($row = mysql_fetch_array($res)) {
+  var_dump($row);
+}
+  return settype($res, "string");
 }
 
 $uid = $_REQUEST['guid'];
