d28e31f53f4427e8c20ac001cd9de0ced0634074
Jonathan Chu <scytop@gmail.com>
2014-01-26 03:04:30 -0800

    Pushed from my OCaml Terminal

--- utils/print_chores.php
+++ utils/print_chores.php
@@ -1,16 +1,20 @@
 <?
 php
-include 'groups.php';
+require_once ('./db_connect.php');
+include '../groups.php';
 function printChores($groupID){
 $array = getChores($groupID);
 
 for ($i = 0; $i < count($array); $i++)
 {
-	echo '<div class="superWrapper" id ="choreID' .$array[$i][chore_id] . '">' .
+	echo '<div class="superWrapper" id ="choreID' .$array[$i][choreid] . '">' .
 		'<div class="choreTitle">' . $array[$i][chore_name]' </div>' .  '<div class="choreDescription">' . $array[$i][chore_description]' </div>' .
 		 '<div class="choreDifficulty">' . $array[$i][difficulty]' </div>' .   '<div class="choreLength">' . $array[$i][length]' </div>' . 
 		  '<div class="choreSugPoints">' . $array[$i][sugPoints]' </div>' .  '<div class="choreActualPoints">' . $array[$i][act_points]' </div>' . 
 }		 '<div class="choreDueDate">' . $array[$i][due_date]' </div></div>'; 
 }
 
+
+//TEST CODE. REMOVE BEFORE EVERYTHING GOES BAD.
+printChores(261)
 ?>
