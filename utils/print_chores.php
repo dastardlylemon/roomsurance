4abe4f31736e7c1aaf36cb676e896fc424b0d9e3
Jonathan Chu <scytop@gmail.com>
2014-01-26 03:16:03 -0800

    testing code

--- utils/print_chores.php
+++ utils/print_chores.php
@@ -13,9 +13,9 @@ for ($i = 0; $i < count($array); $i++)
 		'<div class="choreTitle">' . $array[$i][chore_name] . ' </div>' .  '<div class="choreDescription">' . $array[$i][chore_description] .' </div>' .
 		 '<div class="choreDifficulty">' . $array[$i][difficulty] . ' </div>' .   '<div class="choreLength">' . $array[$i][length] .' </div>' . 
 		  '<div class="choreSugPoints">' . $array[$i][sugPoints].' </div>' .  '<div class="choreActualPoints">' . $array[$i][act_points] .' </div>' . 
-}		 '<div class="choreDueDate">' . $array[$i][due_date].' </div></div>'; 
+		 '<div class="choreDueDate">' . $array[$i][due_date].' </div></div>'; 
 }
-
+}
 
 //TEST CODE. REMOVE BEFORE EVERYTHING GOES BAD.
 printChores(261);
