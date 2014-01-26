737f91fc0b1aab2b8145bb300eaf5300506e7204
dastardlylemon <jwu@efunda.com>
2014-01-26 06:20:44 -0800

    redirect

--- utils/join_group.php
+++ utils/join_group.php
@@ -21,18 +21,18 @@ $groupID = mysql_fetch_assoc($getGroupID);
 $groupID = $groupID['groupid'];
 //if (!$groupID) echo "Could not find group with pass";
 $findUser = "UPDATE users SET usergroup = " . $groupID . " WHERE userid = '" . $userID . "'";
-if (mysql_query($findUser))
+//if (mysql_query($findUser))
  // echo "Successfully joined group";
-else
+//else
 //  echo "Error joining the group: " . mysql_error($db);
 $getCash = "SELECT cash_per FROM groups WHERE groupid = " . $groupID;
 $getCashVal = mysql_query($getCash);
 $cashVal = mysql_fetch_assoc($getCashVal);
 $cashVal = $cashVal['cashval'];
 $putCash = "UPDATE users SET cash = " . $cashVal . " AND points = 1000 WHERE userid = '" . $userID . "'";
-if (mysql_query($putCash))
+//if (mysql_query($putCash))
 //  echo "You now have $" . $cashVal;
-else
+//else
  // echo "Could not add cash";
 calculateTotalPoints($getGroupID);
 
