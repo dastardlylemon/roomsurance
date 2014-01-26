a787db4a86511e4d528c7c32c89864bfc6c85941
dastardlylemon <jwu@efunda.com>
2014-01-26 06:19:07 -0800

    redirect

--- utils/join_group.php
+++ utils/join_group.php
@@ -19,21 +19,21 @@ $findGroup = "SELECT groupid FROM groups WHERE pass = '" . $groupPW . "' LIMIT 1
 $getGroupID = mysql_query($findGroup);
 $groupID = mysql_fetch_assoc($getGroupID);
 $groupID = $groupID['groupid'];
-if (!$groupID) echo "Could not find group with pass";
+//if (!$groupID) echo "Could not find group with pass";
 $findUser = "UPDATE users SET usergroup = " . $groupID . " WHERE userid = '" . $userID . "'";
 if (mysql_query($findUser))
-  echo "Successfully joined group";
+ // echo "Successfully joined group";
 else
-  echo "Error joining the group: " . mysql_error($db);
+//  echo "Error joining the group: " . mysql_error($db);
 $getCash = "SELECT cash_per FROM groups WHERE groupid = " . $groupID;
 $getCashVal = mysql_query($getCash);
 $cashVal = mysql_fetch_assoc($getCashVal);
 $cashVal = $cashVal['cashval'];
 $putCash = "UPDATE users SET cash = " . $cashVal . " AND points = 1000 WHERE userid = '" . $userID . "'";
 if (mysql_query($putCash))
-  echo "You now have $" . $cashVal;
+//  echo "You now have $" . $cashVal;
 else
-  echo "Could not add cash";
+ // echo "Could not add cash";
 calculateTotalPoints($getGroupID);
 
 return $groupID;
