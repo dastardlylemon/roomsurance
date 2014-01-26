386704883a2b26ce7fc5286eff93016bf46f98cf
dastardlylemon <jwu@efunda.com>
2014-01-26 04:02:24 -0800

    reorganized files

---
+++ utils/print_chores.php
@@ -0,0 +1,26 @@
+<?php
+require_once ('../utils/db_connect.php');
+function getChores($groupID)
+{
+	$arr = array();
+	$findChores = "SELECT * FROM chores WHERE taskgroup = " . $groupID;
+	$result = mysql_query($findChores);
+	while ($row = mysql_fetch_assoc($result))
+		$arr[] = $row;
+	return $arr;
+}
+function printChores($groupID){
+$array = getChores($groupID);
+for ($i = 0; $i < count($array); $i++)
+{
+	echo '<div class="superWrapper" id ="choreID' .$array[$i][choreid] . '">' .
+		'<div class="choreTitle">' . $array[$i][chore_name] . ' </div>' .  '<div class="choreDescription">' . $array[$i][chore_description] .' </div>' .
+		 '<div class="choreDifficulty">' . $array[$i][difficulty] . ' </div>' .   '<div class="choreLength">' . $array[$i][length] .' </div>' . 
+		  '<div class="choreSugPoints">' . $array[$i][sugPoints].' </div>' .  '<div class="choreActualPoints">' . $array[$i][act_points] .' </div>' . 
+		 '<div class="choreDueDate">' . $array[$i][due_date].' </div></div>'; 
+}
+}
+
+//TEST CODE. REMOVE BEFORE EVERYTHING GOES BAD.
+printChores(261);
+?>
