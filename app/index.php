249b2041af75abcf92e5569daed3fc584ef6d4af
dastardlylemon <jwu@efunda.com>
2014-01-26 05:46:15 -0800

    group name update

--- app/index.php
+++ app/index.php
@@ -12,11 +12,12 @@ function getChores($groupID)
 
 function printChores($groupID){
   $array = getChores($groupID);
-  if ($array[$i][taskuser]) 
-    $owner = $array[$i][taskuser];
-  else
-    $owner = 'no one';
   for ($i = 0; $i < count($array); $i++) {
+    if ($array[$i][taskuser]) 
+      $owner = $array[$i][taskuser];
+    else
+      $owner = 'no one';
+
     echo '<div class="chore" id ="choreID' .$array[$i][choreid] . '">' .
       '<div class="chore-date">' . date('M', strtotime($array[$i][due_date])) .'<br><b>' . date('d', strtotime($array[$i][due_date])) . '</b></div>' .
       '<h4 class="chore-title">' . $array[$i][chore_name] . ' </h4>' .  
@@ -31,7 +32,7 @@ function printChores($groupID){
 function getGroupName($groupID){
   $findGroup = "SELECT group_name FROM groups WHERE groupid = " . $groupID;
   $res = mysql_query($findGroup);
-  echo $res[0][group_name];
+  echo $res[0]["group_name"];
 }
 
 $uid = $_REQUEST['guid'];
