a2528ad995a4eeabb1df123b370e6481c46d3688
Soomin Jeong <sjeongus@gmail.com>
2014-01-25 18:27:39 -0800

    Fixed removeGroup function

--- groups.php
+++ groups.php
@@ -86,18 +86,22 @@ function joinGroup($userID, $groupPW)
 	calculateTotalPoints($getGroupID);
 }
 
-function removeGroup($groupName)
+function removeGroup($groupID)
 {
-	$didItWork = mysqli_query("DELETE FROM groups WHERE groupid= $groupName");
+	$removeChores = "DELETE FROM chores WHERE taskgroup = " . $groupID;
+	$delete = mysql_query($removeChores);
+	$updateGroup = "UPDATE users SET usergroup = '' WHERE usergroup = " . $groupID;
+	$didItWork = mysqli_query("DELETE FROM groups WHERE groupid = " . $groupID);
 	if($didItWork)
 		echo "Group Deleted.  Come back next time!";
 	else
 		echo "Something went wrong! Please try again in a couple of minutes";
 }
 
+// User passes in the groupid of the group they are in
 function getGroup($groupID)
 {
-	
+
 }
 
 function createChore($choreName, $pointValue, $groupID, $userID = "0", $chore_description = "", $diffi = 0, $timer = 0, $sugPoints = 0,
@@ -142,7 +146,7 @@ function calculateEndValue($totalPoints, $userPoints, $totalGroupCash)
 
 function calculateTotalPoints($groupID)
 {
-	$sqlCall = "SELECT ALL * FROM users WHERE groupid = " . $groupID;
+	$sqlCall = "SELECT * FROM users WHERE groupid = " . $groupID;
 	$result = mysql_query($sqlCall);
 	$points = 0;
 	while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
